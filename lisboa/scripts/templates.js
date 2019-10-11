1b701a1e29f56cf34d8d0673967c7297451c6bae
tallon.simmons <tallon.simmons@nocti.org>
2019-10-11 11:14:23 -0400

    Moved controls to the top of page

--- lisboa/scripts/templates.js
+++ lisboa/scripts/templates.js
@@ -1,5 +1,5 @@
 export default class Templates {
-  getActionHtml = (action) => {
+  getActionHtml = action => {
     if (!action) {
       return "";
     }
@@ -21,131 +21,130 @@ export default class Templates {
     return html;
   };
 
-  courtier = (viewModel) => {
+  courtier = viewModel => {
     if (!viewModel.currentNoble) {
       return "";
     }
 
     let html = `
+    <div id="controlsContainer" class="col text-center mt-2">
+    <div class="btn-group" role="group" aria-label="Basic example">
+      <button type="button" class="btn btn-secondary" disabled>Back</button>
+      <button type="button" class="btn btn-secondary" data-action="step" data-step-to="state">Next</button>
+    </div>
+    </div>
   <div  class="col-12 text-center mt-3 mt-lg-0 slide-in" style="font-size: 1.8vh;">
     <img src="./content/images/${viewModel.currentNoble.image}" class="img-fluid" style="max-height: 60vh;" />
     <p class="badge-dark col-12 col-lg-6 mx-auto rounded">Lacerda's courtier visits ${viewModel.currentNoble.title}</p>
   </div>
-  <div>
-  <div id="controlsContainer" class="col text-center mt-2">
-  <div class="btn-group" role="group" aria-label="Basic example">
-    <button type="button" class="btn btn-secondary" disabled>Back</button>
-    <button type="button" class="btn btn-secondary" data-action="step" data-step-to="state">Next</button>
-  </div>
-  </div>`;
+  <div>`;
 
     return html;
   };
 
-  state = (viewModel) => {
+  state = viewModel => {
     if (!viewModel.currentStateAction) {
       return "";
     }
 
-    const stateActionTemplate = `  
+    const stateActionTemplate = ` 
+    <div id="controlsContainer" class="col text-center mt-2">
+    <div class="btn-group" role="group" aria-label="Basic example">
+      <button type="button" class="btn btn-secondary" data-action="step"  data-step-to="courtier">Back</button>
+      <button type="button" class="btn btn-secondary" data-action="step" data-step-to="noble">Next</button>
+    </div>
+    </div> 
     <div class="col-12 text-center mt-3 mt-lg-0 slide-in">
     <div class="text-center">
       ${this.getActionHtml(viewModel.currentStateAction)}
     </div>
-  </div>
-  <div id="controlsContainer" class="col text-center mt-2">
-  <div class="btn-group" role="group" aria-label="Basic example">
-    <button type="button" class="btn btn-secondary" data-action="step"  data-step-to="courtier">Back</button>
-    <button type="button" class="btn btn-secondary" data-action="step" data-step-to="noble">Next</button>
-  </div>
   </div>`;
 
     return stateActionTemplate;
   };
 
-  noble = (viewModel) => {
+  noble = viewModel => {
     if (!viewModel.currentNobleAction) {
       return "";
     }
 
-    const nobleActionTemplate = `      
+    const nobleActionTemplate = `  
+
+    <div id="controlsContainer" class="col text-center mt-2">
+    <div class="btn-group" role="group" aria-label="Basic example">
+      <button type="button" class="btn btn-secondary" data-action="step"  data-step-to="state">Back</button>
+      <button type="button" class="btn btn-secondary" data-action="step" data-step-to="discardDeck">Next</button>
+    </div>
+    </div>    
   <div class="col-12 text-center mt-3 mt-lg-0 slide-in">
   <div class="text-center">
     ${this.getActionHtml(viewModel.currentNobleAction)}
   </div>
-</div>
-
-<div id="controlsContainer" class="col text-center mt-2">
-<div class="btn-group" role="group" aria-label="Basic example">
-  <button type="button" class="btn btn-secondary" data-action="step"  data-step-to="state">Back</button>
-  <button type="button" class="btn btn-secondary" data-action="step" data-step-to="discardDeck">Next</button>
-</div>
 </div>`;
 
     return nobleActionTemplate;
   };
 
-  discardDeck = (viewModel) => {
+  discardDeck = viewModel => {
     if (!viewModel.currentDeck) {
       return "";
     }
 
     const discardDeckTemplate = `
-    <div class="col-12 text-center mt-3 mt-lg-0 slide-in" style="font-size: 1.8vh;">
-    <img src="./content/images/${viewModel.currentDeck.image}" class="img-fluid rounded mx-auto d-block mt-3 mb-2" style="max-height: 60vh;" />
-    <p class="badge-dark col-12 col-lg-6 mx-auto mt-2 rounded">Lacerda discards from the ${viewModel.currentDeck.title} (if able)</p>
-    </div>
     <div id="controlsContainer" class="col text-center mt-2">
     <div class="btn-group" role="group" aria-label="Basic example">
       <button type="button" class="btn btn-secondary" data-action="step"  data-step-to="noble">Back</button>
       <button type="button" class="btn btn-secondary" data-action="step" data-step-to="moveDeck">Next</button>
     </div>
+    </div>
+    <div class="col-12 text-center mt-3 mt-lg-0 slide-in" style="font-size: 1.8vh;">
+    <img src="./content/images/${viewModel.currentDeck.image}" class="img-fluid rounded mx-auto d-block mt-3 mb-2" style="max-height: 60vh;" />
+    <p class="badge-dark col-12 col-lg-6 mx-auto mt-2 rounded">Lacerda discards from the ${viewModel.currentDeck.title} (if able)</p>
     </div>`;
 
     return discardDeckTemplate;
   };
 
-  moveDeck = (viewModel) => {
+  moveDeck = viewModel => {
     if (!viewModel.currentDeck) {
       return "";
     }
 
     const moveDeckTemplate = `
-    <div class="col-12 text-center mt-3 mt-lg-0 slide-in" style="font-size: 1.8vh;">
-    <img src="./content/images/${viewModel.currentDeck.image}" class="img-fluid rounded mx-auto d-block mt-3 mb-2" style="max-height: 60vh;" />
-    <p class="badge-dark col-12 col-lg-6 mx-auto mt-2 rounded">Lacerda's helper moves to the ${viewModel.currentDeck.title} (if able, otherwise skip deck)</p>
-    </div>
     <div id="controlsContainer" class="col text-center mt-2">
     <div class="btn-group" role="group" aria-label="Basic example">
       <button type="button" class="btn btn-secondary" data-action="step"  data-step-to="previousDiscardDeck">Back</button>
       <button type="button" class="btn btn-secondary" data-action="skipDeck">Skip Deck</button>
       <button type="button" class="btn btn-secondary" data-action="nextAction">Next</button>
     </div>
+    </div>
+    <div class="col-12 text-center mt-3 mt-lg-0 slide-in" style="font-size: 1.8vh;">
+    <img src="./content/images/${viewModel.currentDeck.image}" class="img-fluid rounded mx-auto d-block mt-3 mb-2" style="max-height: 60vh;" />
+    <p class="badge-dark col-12 col-lg-6 mx-auto mt-2 rounded">Lacerda's helper moves to the ${viewModel.currentDeck.title} (if able, otherwise skip deck)</p>
     </div>`;
 
     return moveDeckTemplate;
   };
 
-  previousDiscardDeck = (viewModel) => {
+  previousDiscardDeck = viewModel => {
     if (!viewModel.currentDeck) {
       return "";
     }
 
     const discardDeckTemplate = `
-    <div class="col-12 text-center mt-3 mt-lg-0 slide-in" style="font-size: 1.8vh;">
-    <img src="./content/images/${viewModel.previousDeck.image}" class="img-fluid rounded mx-auto d-block mt-3 mb-2" style="max-height: 60vh;" />
-    <p class="badge-dark col-12 col-lg-6 mx-auto mt-2 rounded">Lacerda discards from the ${viewModel.previousDeck.title} (if able)</p>
-    </div>
     <div id="controlsContainer" class="col text-center mt-2">
     <div class="btn-group" role="group" aria-label="Basic example">
       <button type="button" class="btn btn-secondary" data-action="step"  data-step-to="noble">Back</button>
       <button type="button" class="btn btn-secondary" data-action="step" data-step-to="moveDeck">Next</button>
     </div>
+    </div>
+    <div class="col-12 text-center mt-3 mt-lg-0 slide-in" style="font-size: 1.8vh;">
+    <img src="./content/images/${viewModel.previousDeck.image}" class="img-fluid rounded mx-auto d-block mt-3 mb-2" style="max-height: 60vh;" />
+    <p class="badge-dark col-12 col-lg-6 mx-auto mt-2 rounded">Lacerda discards from the ${viewModel.previousDeck.title} (if able)</p>
     </div>`;
 
     return discardDeckTemplate;
-
-  }
+  };
 
   getSetupHtml(currentDeck) {
     let html = "";
