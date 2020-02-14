264f780c2acd8645cd3f5d7651e4cf5d1f7d085c
Tallon Simmons <tallon.simmons@icloud.com>
2020-02-14 18:10:15 -0500

    Transpile/minify dino island js to solve safari support issues

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,12 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        lisboa: "./lisboa/scripts/app.js"
+        lisboa: "./lisboa/scripts/app.js",
+        dinosaurIsland: "./cards/dinosaurisland/scripts/app.js"
     },
     output: {
-        path: path.resolve(__dirname, './lisboa/scripts/build'),
-        filename: 'lisboa.js'
+        path: path.resolve(__dirname, './scripts/build'),
+        filename: '[name].js'
     },
     // IMPORTANT NOTE: If you are using Webpack 2 or above, replace "loaders" with "rules"
     module: {
