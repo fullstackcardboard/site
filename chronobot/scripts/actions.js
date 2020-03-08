ca2e05430885d96cd478128e46679300ce2682c2
Tallon Simmons <tallon.simmons@icloud.com>
2020-03-08 12:36:29 -0400

    Updated action size to better fit smaller screens

--- chronobot/scripts/actions.js
+++ chronobot/scripts/actions.js
@@ -18,14 +18,14 @@ const ActionsDisplayComponent = function() {
       if (actions[key].triggers.length > 0) {
         for (let index = 0; index < actions[key].triggers.length; index++) {
           const number = actions[key].triggers[index];
-          triggerNumbers += `<img src='content/images/trigger_${number}.png' class="rounded-circle" style="height: 7vh;" />`;
+          triggerNumbers += `<img src='content/images/trigger_${number}.png' class="rounded-circle" style="height: 5vh;" />`;
         }
       }
 
       html += `<div
-            class="col-xs-12 col-sm-6 border shadow rounded bg-dark text-white action text-center mb-2"
+            class="col-xs-12 col-sm-6 border shadow rounded bg-dark text-white action text-center mb-1"
             data-action="${key}"
-            style="height: 12vh;"
+            style="height: 8vh;"
           >
               ${getImageHtml(key)} ${triggerNumbers}
           </div>`;
@@ -46,13 +46,13 @@ const ActionsDisplayComponent = function() {
         const action = actions[key];
         for (let index = 0; index < actions[key].triggers.length; index++) {
           const number = actions[key].triggers[index];
-          triggerNumbers += `<img src='content/images/trigger_${number}.png' class="rounded-circle mr-1" style="height: 7vh;" />`;
+          triggerNumbers += `<img src='content/images/trigger_${number}.png' class="rounded-circle mr-1" style="height: 5vh;" />`;
         }
         html += `
             <div
               class="col-xs-12 col-md-6 mb-2 border shadow rounded bg-dark text-white action text-center"
               data-action="${key}"
-              style="height: 10vh;"
+              style="height: 8vh;"
             >
               ${getImageHtml(key)} ${triggerNumbers}
             </div>
@@ -67,38 +67,38 @@ const ActionsDisplayComponent = function() {
     switch (key) {
       case "lab":
         return `
-        <img src="${baseImageUrl}construct.png" style="height: 7vh;" />
-        <img src="${baseImageUrl}lab.png" style="height: 7vh;" />
+        <img src="${baseImageUrl}construct.png" style="height: 5vh;" />
+        <img src="${baseImageUrl}lab.png" style="height: 5vh;" />
       `;
       case "power":
         return `
-        <img src="${baseImageUrl}construct.png" style="height: 7vh;" />
-        <img src="${baseImageUrl}power.png" style="height: 7vh;" />
+        <img src="${baseImageUrl}construct.png" style="height: 5vh;" />
+        <img src="${baseImageUrl}power.png" style="height: 5vh;" />
       `;
       case "support":
         return `
-        <img src="${baseImageUrl}construct.png" style="height: 7vh;" />
-        <img src="${baseImageUrl}support.png" style="height: 7vh;" />
+        <img src="${baseImageUrl}construct.png" style="height: 5vh;" />
+        <img src="${baseImageUrl}support.png" style="height: 5vh;" />
       `;
       case "factory":
         return `
-        <img src="${baseImageUrl}construct.png" style="height: 7vh;" />
-        <img src="${baseImageUrl}factory.png" style="height: 7vh;" />
+        <img src="${baseImageUrl}construct.png" style="height: 5vh;" />
+        <img src="${baseImageUrl}factory.png" style="height: 5vh;" />
       `;
       case "super":
         return `
-        <img src="${baseImageUrl}construct.png" style="height: 7vh;" />
-        <img src="${baseImageUrl}super.png" style="height: 7vh;" />
+        <img src="${baseImageUrl}construct.png" style="height: 5vh;" />
+        <img src="${baseImageUrl}super.png" style="height: 5vh;" />
       `;
       case "supply":
         return `
-        <img src="${baseImageUrl}supply.png" style="height: 7vh;" />
-        <img src="${baseImageUrl}recruit.png" style="height: 7vh;" />
+        <img src="${baseImageUrl}supply.png" style="height: 5vh;" />
+        <img src="${baseImageUrl}recruit.png" style="height: 5vh;" />
       `;
       default:
       case "power":
         return `
-        <img src="${baseImageUrl}${key}.png" style="height: 7vh;" />
+        <img src="${baseImageUrl}${key}.png" style="height: 5vh;" />
       `;
     }
   }
